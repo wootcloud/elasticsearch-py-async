8caea39879aedd10b229eae692baacb873f9bef9
Honza Král <honza.kral@gmail.com>
2016-03-12 02:45:27 +0100

    Initial commit, working connection

---
+++ elasticsearch_async/connection.py
@@ -0,0 +1,75 @@
+import asyncio
+import time
+
+import aiohttp
+from aiohttp.errors import FingerprintMismatch, ClientError
+
+from elasticsearch.exceptions import ConnectionError, ConnectionTimeout, SSLError
+from elasticsearch.connection import Connection
+from elasticsearch.compat import urlencode
+
+class AIOHttpConnection(Connection):
+    def __init__(self, host='localhost', port=9200, http_auth=None,
+            use_ssl=False, verify_certs=False, ca_certs=None, client_cert=None,
+            client_key=None, **kwargs):
+        super().__init__(host=host, port=port, **kwargs)
+
+        if http_auth is not None:
+            if isinstance(http_auth, str):
+                http_auth = tuple(http_auth.split(':', 1))
+
+            if isinstance(http_auth, (tuple, list)):
+                http_auth = aiohttp.BasicAuth(*http_auth)
+
+        self.session = aiohttp.ClientSession(
+            auth=http_auth,
+            connector=aiohttp.TCPConnector(
+                verify_ssl=verify_certs,
+                conn_timeout=self.timeout,
+
+            )
+        )
+
+        self.base_url = 'http%s://%s:%d%s' % (
+            's' if use_ssl else '',
+            host, port, self.url_prefix
+        )
+
+    @asyncio.coroutine
+    def perform_request(self, method, url, params=None, body=None, timeout=None, ignore=()):
+        url_path = url
+        if params:
+            url_path = '%s?%s' % (url, urlencode(params or {}))
+        url = self.base_url + url_path
+
+        start = time.time()
+        response = None
+        try:
+            response = yield from asyncio.wait_for(self.session.request(method, url, data=body), timeout or self.timeout)
+            raw_data = yield from response.text()
+            duration = time.time() - start
+
+        except asyncio.TimeoutError as e:
+            self.log_request_fail(method, url, body, time.time() - start, exception=e)
+            raise ConnectionTimeout('TIMEOUT', str(e), e)
+
+        except FingerprintMismatch as e:
+            self.log_request_fail(method, url, body, time.time() - start, exception=e)
+            raise SSLError('N/A', str(e), e)
+
+        except ClientError as e:
+            self.log_request_fail(method, url, body, time.time() - start, exception=e)
+            raise ConnectionError('N/A', str(e), e)
+
+        finally:
+            if response is not None:
+                yield from response.release()
+
+        # raise errors based on http status codes, let the client handle those if needed
+        if not (200 <= response.status < 300) and response.status not in ignore:
+            self.log_request_fail(method, url, body, duration, response.status, raw_data)
+            self._raise_error(response.status, raw_data)
+
+        self.log_request_success(method, url, url_path, body, response.status, raw_data, duration)
+
+        return response.status, response.headers, raw_data
---
+++ test_elasticsearch_async/test_connection.py
@@ -0,0 +1,59 @@
+import asyncio
+import json
+import logging
+
+import aiohttp
+
+from pytest import mark, yield_fixture, raises
+
+from elasticsearch import NotFoundError
+
+from elasticsearch_async.connection import AIOHttpConnection
+
+@yield_fixture
+def connection(event_loop):
+    connection = AIOHttpConnection()
+    yield connection
+    event_loop.run_until_complete(asyncio.ensure_future(connection.session.close()))
+
+@mark.asyncio
+def test_info(connection):
+    status, headers, data = yield from connection.perform_request('GET', '/')
+
+    data = json.loads(data)
+
+    assert status == 200
+    assert data['tagline'] == "You Know, for Search"
+
+def test_auth_is_set_correctly():
+    connection = AIOHttpConnection(http_auth=('user', 'secret'))
+    assert connection.session._default_auth == aiohttp.BasicAuth('user', 'secret')
+
+    connection = AIOHttpConnection(http_auth='user:secret')
+    assert connection.session._default_auth == aiohttp.BasicAuth('user', 'secret')
+
+@mark.asyncio
+def test_request_is_properly_logged(connection, caplog):
+    yield from connection.perform_request('GET', '/_cat/indices', body=b'{}', params={"format": "json"})
+
+    for logger, level, message in caplog.record_tuples:
+        if logger == 'elasticsearch' and level == logging.INFO:
+            assert message.startswith('GET http://localhost:9200/_cat/indices?format=json [status:200 request:')
+            break
+    else:
+        assert False, 'Message not found'
+
+    assert ('elasticsearch', logging.DEBUG, '> {}') in caplog.record_tuples
+    assert ('elasticsearch', logging.DEBUG, '< []') in caplog.record_tuples
+
+@mark.asyncio
+def test_error_is_properly_logged(connection, caplog):
+    with raises(NotFoundError):
+        yield from connection.perform_request('GET', '/not-here', params={"some": "data"})
+
+    for logger, level, message in caplog.record_tuples:
+        if logger == 'elasticsearch' and level == logging.WARNING:
+            assert message.startswith('GET http://localhost:9200/not-here?some=data [status:404 request:')
+            break
+    else:
+        assert False, "Log not received"
