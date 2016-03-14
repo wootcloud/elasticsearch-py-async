0a7f0e43e24a54296e4b9dfcc41d3c3c563d9dd9
Honza Král <honza.kral@gmail.com>
2016-03-14 23:29:45 +0100

    Add test for body/params

--- test_elasticsearch_async/test_client.py
+++ test_elasticsearch_async/test_client.py
@@ -24,3 +24,23 @@ def test_exists_with_404_returns_false(client):
 def test_404_properly_raised(client):
     with raises(NotFoundError):
         yield from client.get(index='not-there', doc_type='t', id=42)
+
+@mark.asyncio
+def test_params_and_body_get_passed_properly(client):
+    response = {
+        'tokens': [
+            {
+                'end_offset': 12,
+                'position': 0,
+                'start_offset': 0,
+                'token': 'Hello World!',
+                'type': 'word'
+            }
+        ]
+    }
+
+    data = yield from client.indices.analyze(body={'analyzer': 'keyword', 'text': 'Hello World!'})
+    assert data == response
+
+    data = yield from client.indices.analyze(**{'analyzer': 'keyword', 'text': 'Hello World!'})
+    assert data == response
