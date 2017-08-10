761f605309f7c4972892f2ad4e6da8e907fe2bcf
Honza Král <honza.kral@gmail.com>
2017-08-10 15:15:29 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from os.path import join, dirname
 from setuptools import setup, find_packages
 
-VERSION = (5, 1, 0)
+VERSION = (5, 2, 0)
 __version__ = VERSION
 __versionstr__ = '.'.join(map(str, VERSION))
 
